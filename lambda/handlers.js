253137b63a30ab9c451806e53a6666b676ec1eb2
Dan Schoppe <dan.schoppe@gmail.com>
2020-05-14 01:14:00 -0500

    Update Lambda

--- lambda/handlers.js
+++ lambda/handlers.js
@@ -1,39 +1,63 @@
 var aws = require('aws-sdk')
 
+const toStateChangeReport = (topKey) => (data) => {
+  const previousState = data[topKey][0].PreviousState.Name
+  const currentState = data[topKey][0].CurrentState.Name
+  return `${previousState} -> ${currentState}`
+}
+const toStatusReport = (data) => data.InstanceStatuses[0].InstanceState.Name
+
 // Start instance
-exports.start = (event, context) => {
-  return _instanceState(event, context, 'startInstances')
+exports.start = (_event, _context, callback) => {
+  const method = 'startInstances'
+  const dataTransform = toStateChangeReport('StartingInstances')
+  return _instanceState({ callback, method, dataTransform })
 }
 
 // Stop instance
-exports.stop = (event, context) => {
-  return _instanceState(event, context, 'stopInstances')
+exports.stop = (_event, _context, callback) => {
+  const method = 'stopInstances'
+  const dataTransform = toStateChangeReport('StoppingInstances')
+  return _instanceState({ callback, method, dataTransform })
 }
 
 // Get instance status
-exports.status = (event, context) => {
-  return _instanceState(event, context, 'describeInstanceStatus')
+exports.status = (_event, _context, callback) => {
+  const method = 'describeInstanceStatus'
+  const dataTransform = toStatusReport
+  const methodParams = { IncludeAllInstances: true }
+  return _instanceState({ callback, method, methodParams, dataTransform })
 }
 
-const _instanceState = (event, context, method) => {
+const _instanceState = ({
+  callback,
+  method,
+  methodParams = {},
+  dataTransform = (data) => data
+}) => {
   const instanceId = process.env.INSTANCE_ID
   if (!instanceId) {
     const error = 'No instanceId provided'
     console.error(error)
-    return context.fail(error)
+    return callback(new Error(error))
   }
 
   aws.config.update({region: 'us-east-1'})
   const params = {
-    InstanceIds: [ instanceId ],
+    ...methodParams,
+    InstanceIds: [ instanceId ]
   }
 
   new aws.EC2()[method](params, function(error, data) {
     if (error) {
       console.error(error)
-      context.fail(error)
+      callback(new Error(error))
+      return
     }
 
-    context.succeed(data)
+    const statusCode = 200
+    const body = dataTransform(data)
+    console.log(body)
+    callback(null, { statusCode, body })
   })
 }
